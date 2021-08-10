1c56358baf12ae77875d6a296f7a7a4af947e304
Minju Ko <70134713+Jerry-ko@users.noreply.github.com>
2021-08-10 14:08:32 +0900

    로딩 효과 적용하기

--- js/main.js
+++ js/main.js
@@ -540,6 +540,7 @@
     })
     //window.addEventListener('DOMContentLoaded', setLayout)
     window.addEventListener('load', () => {
+        document.body.classList.remove('before-load')
         setLayout();
         sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.videoImages[0], 0, 0);
     });
@@ -550,6 +551,9 @@
         sceneInfo[3].values.rectStartY = 0;
     });
     window.addEventListener('orientationchange', setLayout)
+    document.querySelector('.loading').addEventListener('transitionend', (e) => {
+        document.body. removeChild(e.currentTarget)
+    })
 
     setCanvasImages();
 
