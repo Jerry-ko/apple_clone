d376e8fa235c2fd890495da30a64bc1cf3a3a6f5
Minju Ko <70134713+Jerry-ko@users.noreply.github.com>
2021-08-11 14:54:05 +0900

    수정사항 반영

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 (() => {
 
-    let yOffset = 0; // window.pageYOffset 대신 쓸 변수
-    let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
-    let currentScene = 0; // 현재 활성화된(눈 앞에 보고 있는) 씬(scroll-section)
-    let enterNewScene = false; //새로운 scene이 시작된 순간 true
-    let acc = 0.2;
-    let delayedYOffset = 0;
-    let rafId;
-    let rafState;
+	let yOffset = 0; // window.pageYOffset 대신 쓸 변수
+	let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+	let currentScene = 0; // 현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
+	let enterNewScene = false; // 새로운 scene이 시작된 순간 true
+	let acc = 0.2;
+	let delayedYOffset = 0;
+	let rafId;
+	let rafState;
 
-    const sceneInfo = [
-        {
-            //0
-            type: 'sticky',
+	const sceneInfo = [
+		{
+			// 0
+			type: 'sticky',
 			heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
 			scrollHeight: 0,
 			objs: {
@@ -45,22 +45,21 @@
 				messageB_translateY_out: [0, -20, { start: 0.45, end: 0.5 }],
 				messageC_translateY_out: [0, -20, { start: 0.65, end: 0.7 }],
 				messageD_translateY_out: [0, -20, { start: 0.85, end: 0.9 }]
-            }
-
-        },
-        {
-            //1
-            type: 'normal',
+			}
+		},
+		{
+			// 1
+			type: 'normal',
 			// heightNum: 5, // type normal에서는 필요 없음
 			scrollHeight: 0,
 			objs: {
 				container: document.querySelector('#scroll-section-1'),
 				content: document.querySelector('#scroll-section-1 .description')
 			}
-        },
-        {
-            //2
-            type: 'sticky',
+		},
+		{
+			// 2
+			type: 'sticky',
 			heightNum: 5,
 			scrollHeight: 0,
 			objs: {
@@ -93,11 +92,11 @@
 				messageC_opacity_out: [1, 0, { start: 0.95, end: 1 }],
 				pinB_scaleY: [0.5, 1, { start: 0.6, end: 0.65 }],
 				pinC_scaleY: [0.5, 1, { start: 0.87, end: 0.92 }]
-            }
-        },
-        {
-            //3
-            type: 'sticky',
+			}
+		},
+		{
+			// 3
+			type: 'sticky',
 			heightNum: 5,
 			scrollHeight: 0,
 			objs: {
@@ -120,97 +119,96 @@
 				canvasCaption_translateY: [ 20, 0, { start: 0, end: 0 } ],
 				rectStartY: 0
 			}
-        }
-    ]
+		}
+	];
 
-    function setCanvasImages() {
-        let imgElem;
-        for (let i = 0; i < sceneInfo[0].values.videoImageCount; i++) {
-            imgElem = new Image();
-            imgElem.src = `./video/001/IMG_${6726 + i}.JPG`;
-            sceneInfo[0].objs.videoImages.push(imgElem)
-        }
+	function setCanvasImages() {
+		let imgElem;
+		for (let i = 0; i < sceneInfo[0].values.videoImageCount; i++) {
+			imgElem = new Image();
+			imgElem.src = `./video/001/IMG_${6726 + i}.JPG`;
+			sceneInfo[0].objs.videoImages.push(imgElem);
+		}
 
-        let imgElem2;
-        for (let i = 0; i < sceneInfo[2].values.videoImageCount; i++) {
-            imgElem2 = new Image();
-            imgElem2.src = `./video/002/IMG_${7027 + i}.JPG`;
-            sceneInfo[2].objs.videoImages.push(imgElem2)
-        }
+		let imgElem2;
+		for (let i = 0; i < sceneInfo[2].values.videoImageCount; i++) {
+			imgElem2 = new Image();
+			imgElem2.src = `./video/002/IMG_${7027 + i}.JPG`;
+			sceneInfo[2].objs.videoImages.push(imgElem2);
+		}
 
-        let imgElem3;
-        for (let i = 0; i < sceneInfo[3].objs.imagesPath.length; i++) {
-            imgElem3 = new Image();
-            imgElem3.src = sceneInfo[3].objs.imagesPath[i];
-            sceneInfo[3].objs.images.push(imgElem3)
-        }
-    }
-   
+		let imgElem3;
+		for (let i = 0; i < sceneInfo[3].objs.imagesPath.length; i++) {
+			imgElem3 = new Image();
+			imgElem3.src = sceneInfo[3].objs.imagesPath[i];
+			sceneInfo[3].objs.images.push(imgElem3);
+		}
+	}
 
-    function checkMenu() {
-        if (yOffset > 44) {
-            document.body.classList.add('local-nav-sticky');
-        } else {
-            document.body.classList.remove('local-nav-sticky');
-        }
-    }
+	function checkMenu() {
+		if (yOffset > 44) {
+			document.body.classList.add('local-nav-sticky');
+		} else {
+			document.body.classList.remove('local-nav-sticky');
+		}
+	}
 
-    function setLayout() {
-        // 각 스크롤 섹션의 높이 세팅
-        for (let i = 0; i < sceneInfo.length; i++) {
-            if (sceneInfo[i].type === 'sticky') {
-                sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
-            } else if (sceneInfo[i].type === 'normal') {
-                sceneInfo[i].scrollHeight = sceneInfo[i].objs.container.offsetHeight
-            }
+	function setLayout() {
+		// 각 스크롤 섹션의 높이 세팅
+		for (let i = 0; i < sceneInfo.length; i++) {
+			if (sceneInfo[i].type === 'sticky') {
+				sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+			} else if (sceneInfo[i].type === 'normal')  {
+				sceneInfo[i].scrollHeight = sceneInfo[i].objs.content.offsetHeight + window.innerHeight * 0.5;
+			}
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
-        }
+		}
 
-        yOffset = window.pageYOffset;
+		yOffset = window.pageYOffset;
 
-        let totalScrollHeight = 0;
-        for (let i = 0; i < sceneInfo.length; i++) {
-            totalScrollHeight += sceneInfo[i].scrollHeight;
-            if (totalScrollHeight >= yOffset) {
-                currentScene = i;
-                break;
-            }
-        }
-        document.body.setAttribute('id', `show-scene-${currentScene}`)
+		let totalScrollHeight = 0;
+		for (let i = 0; i < sceneInfo.length; i++) {
+			totalScrollHeight += sceneInfo[i].scrollHeight;
+			if (totalScrollHeight >= yOffset) {
+				currentScene = i;
+				break;
+			}
+		}
+		document.body.setAttribute('id', `show-scene-${currentScene}`);
 
-        const heightRatio = window.innerHeight / 1080;
-        sceneInfo[0].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`
-        sceneInfo[2].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`
-    }
+		const heightRatio = window.innerHeight / 1080;
+		sceneInfo[0].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
+		sceneInfo[2].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
+	}
 
-    function calcValues(values, currentYOffset) {
-        let rv;
-        //현재 씬(스크롤섹션)에서 스크롤된 범위를 비율로 구하기
-        const scrollHeight = sceneInfo[currentScene].scrollHeight;
-        const scrollRatio = currentYOffset / scrollHeight;
+	function calcValues(values, currentYOffset) {
+		let rv;
+		// 현재 씬(스크롤섹션)에서 스크롤된 범위를 비율로 구하기
+		const scrollHeight = sceneInfo[currentScene].scrollHeight;
+		const scrollRatio = currentYOffset / scrollHeight;
 
-        if (values.langth === 3) {
-            // start ~ end 사이에 애니메이션 실행
-            const partScrollStart = values[2].start * scrollHeight;
-            const partScrollEnd = values[2].end * scrollHeight;
-            const partScrollHeight = partScrollEnd - partScrollStart;
+		if (values.length === 3) {
+			// start ~ end 사이에 애니메이션 실행
+			const partScrollStart = values[2].start * scrollHeight;
+			const partScrollEnd = values[2].end * scrollHeight;
+			const partScrollHeight = partScrollEnd - partScrollStart;
 
-            if (currentYOffset >= partScrollStart && currentYOffset <= partScrollEnd) {
-                rv = (currentYOffset - partScrollStart) / partScrollHeight * (values[1] - values[0]) + values[0]
-            } else if (currentYOffset < partScrollStart) {
-                rv = values[0]
-            } else if (currentYOffset > partScrollEnd) {
-                rv = values[1]
-            }
-        } else {
-            rv = scrollRatio * (values[1] - values[0]) + values[0]
-        }
+			if (currentYOffset >= partScrollStart && currentYOffset <= partScrollEnd) {
+				rv = (currentYOffset - partScrollStart) / partScrollHeight * (values[1] - values[0]) + values[0];
+			} else if (currentYOffset < partScrollStart) {
+				rv = values[0];
+			} else if (currentYOffset > partScrollEnd) {
+				rv = values[1];
+			}
+		} else {
+			rv = scrollRatio * (values[1] - values[0]) + values[0];
+		}
 
-        return rv;
-    }
+		return rv;
+	}
 
-    function playAnimation() {
-        const objs = sceneInfo[currentScene].objs;
+	function playAnimation() {
+		const objs = sceneInfo[currentScene].objs;
 		const values = sceneInfo[currentScene].values;
 		const currentYOffset = yOffset - prevScrollHeight;
 		const scrollHeight = sceneInfo[currentScene].scrollHeight;
@@ -465,82 +463,129 @@
 		}
 	}
 
-    function scrollLoop() {
-        enterNewScene = false;
-        prevScrollHeight = 0;
+	function scrollLoop() {
+		enterNewScene = false;
+		prevScrollHeight = 0;
 
-        for (let i = 0; i < currentScene; i++) {
-            prevScrollHeight += sceneInfo[i].scrollHeight
+		for (let i = 0; i < currentScene; i++) {
+			prevScrollHeight += sceneInfo[i].scrollHeight;
+		}
+
+		if (delayedYOffset < prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+			document.body.classList.remove('scroll-effect-end');
+		}
+
+		if (delayedYOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+			enterNewScene = true;
+			if (currentScene === sceneInfo.length - 1) {
+				document.body.classList.add('scroll-effect-end');
+			}
+			if (currentScene < sceneInfo.length - 1) {
+				currentScene++;
+			}
+			document.body.setAttribute('id', `show-scene-${currentScene}`);
+		}
+
+		if (delayedYOffset < prevScrollHeight) {
+			enterNewScene = true;
+			// 브라우저 바운스 효과로 인해 마이너스가 되는 것을 방지(모바일)
+			if (currentScene === 0) return;
+			currentScene--;
+			document.body.setAttribute('id', `show-scene-${currentScene}`);
+		}
+
+		if (enterNewScene) return;
+
+		playAnimation();
+	}
+
+	function loop() {
+		delayedYOffset = delayedYOffset + (yOffset - delayedYOffset) * acc;
+
+		if (!enterNewScene) {
+			if (currentScene === 0 || currentScene === 2) {
+				const currentYOffset = delayedYOffset - prevScrollHeight;
+				const objs = sceneInfo[currentScene].objs;
+				const values = sceneInfo[currentScene].values;
+				let sequence = Math.round(calcValues(values.imageSequence, currentYOffset));
+				if (objs.videoImages[sequence]) {
+					objs.context.drawImage(objs.videoImages[sequence], 0, 0);
+				}
+			}
+		}
+
+        // 일부 기기에서 페이지 끝으로 고속 이동하면 body id가 제대로 인식 안되는 경우를 해결
+        // 페이지 맨 위로 갈 경우: scrollLoop와 첫 scene의 기본 캔버스 그리기 수행
+        if (delayedYOffset < 1) {
+            scrollLoop();
+            sceneInfo[0].objs.canvas.style.opacity = 1;
+            sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.videoImages[0], 0, 0);
+        }
+        // 페이지 맨 아래로 갈 경우: 마지막 섹션은 스크롤 계산으로 위치 및 크기를 결정해야할 요소들이 많아서 1픽셀을 움직여주는 것으로 해결
+        if ((document.body.offsetHeight - window.innerHeight) - delayedYOffset < 1) {
+            let tempYOffset = yOffset;
+            scrollTo(0, tempYOffset - 1);
         }
 
-        if (delayedYOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
-            enterNewScene = true;
-            currentScene++;
-            document.body.setAttribute('id', `show-scene-${currentScene}`)
-        }
+		rafId = requestAnimationFrame(loop);
 
-        if (delayedYOffset < prevScrollHeight) {
-            enterNewScene = true;
-            if (currentScene === 0) return; //브라우저 바운스 효과로 인해 마이너스가 되는 것을 방지(모바일)
-            currentScene--;
-            document.body.setAttribute('id', `show-scene-${currentScene}`)
-        }
+		if (Math.abs(yOffset - delayedYOffset) < 1) {
+			cancelAnimationFrame(rafId);
+			rafState = false;
+		}
+	}
 
-        if (enterNewScene) return;
-
-        playAnimation();
-    }
-
-    function loop() {
-        delayedYOffset = delayedYOffset + (yOffset - delayedYOffset) * acc;
-
-        if (!enterNewScene) {
-            if (currentScene === 0 || currentScene === 2) {
-                const currentYOffset = delayedYOffset - prevScrollHeight;
-                const objs = sceneInfo[currentScene].objs;
-                const values = sceneInfo[currentScene].values;
-                let sequence = Math.round(calcValues(values.imageSequence, currentYOffset));
-                if (objs.videoImages[sequence]) {
-                    objs.context.drawImage(objs.videoImages[sequence], 0, 0);
-                }
-            }
-        }
-
-        rafId = requestAnimationFrame(loop);
-
-        if (Math.abs(yOffset - delayedYOffset) < 1 ) {
-            cancelAnimationFrame(rafId);
-            rafState = false;
-        }
-    }
-
-    window.addEventListener('scroll', () => {
-        yOffset = window.pageYOffset
-        scrollLoop();
-        checkMenu();
-
-        if(!rafState) {
-            rafId = requestAnimationFrame(loop);
-            rafId = true;
-        }
-    })
-    //window.addEventListener('DOMContentLoaded', setLayout)
-    window.addEventListener('load', () => {
-        document.body.classList.remove('before-load')
+	window.addEventListener('load', () => {
+		setLayout(); // 중간에 새로고침 시, 콘텐츠 양에 따라 높이 계산에 오차가 발생하는 경우를 방지하기 위해 before-load 클래스 제거 전에도 확실하게 높이를 세팅하도록 한번 더 실행
+        document.body.classList.remove('before-load');
         setLayout();
         sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.videoImages[0], 0, 0);
-    });
-    window.addEventListener('resize', () => {
-        if (window.innerWidth > 900) {
-            setLayout();
-        }
-        sceneInfo[3].values.rectStartY = 0;
-    });
-    window.addEventListener('orientationchange', setLayout)
-    document.querySelector('.loading').addEventListener('transitionend', (e) => {
-        document.body. removeChild(e.currentTarget)
-    })
 
-    setCanvasImages();
+		// 중간에서 새로고침 했을 경우 자동 스크롤로 제대로 그려주기
+        let tempYOffset = yOffset;
+        let tempScrollCount = 0;
+        if (tempYOffset > 0) {
+            let siId = setInterval(() => {
+                scrollTo(0, tempYOffset);
+                tempYOffset += 5;
+
+                if (tempScrollCount > 20) {
+                    clearInterval(siId);
+                }
+                tempScrollCount++;
+            }, 20);
+        }
+
+        window.addEventListener('scroll', () => {
+            yOffset = window.pageYOffset;
+            scrollLoop();
+  			checkMenu();
+
+  			if (!rafState) {
+  				rafId = requestAnimationFrame(loop);
+  				rafState = true;
+  			}
+  		});
+
+  		window.addEventListener('resize', () => {
+  			if (window.innerWidth > 900) {
+				window.location.reload();
+			}
+  		});
+
+  		window.addEventListener('orientationchange', () => {
+			scrollTo(0, 0);
+			setTimeout(() => {
+				window.location.reload();
+			}, 500);
+  		});
+
+  		document.querySelector('.loading').addEventListener('transitionend', (e) => {
+  			document.body.removeChild(e.currentTarget);
+  		});
+
+	});
+
+	setCanvasImages();
 
 })();
